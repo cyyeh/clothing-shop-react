4d4e187ef666b33586b1ed2ce1bcbb9f9aa977a1
ChihYu Yeh <cyyeh@data-sci.info>
2022-01-31 18:09:41 +0800

    add stripe testing warning text

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -34,6 +34,11 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: Any future date - CVC: Any 3 digits
+    </div>
     <StripeCheckoutButton price={total} />
   </div>
 )
